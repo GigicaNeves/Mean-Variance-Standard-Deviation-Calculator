d7aa86792b90725f3a5dbb2339182c6ca7b83d2f
Giovanna Neves Rodrigues <giovanna.neves@sou.inteli.edu.br>
2025-08-22 04:02:14 +0000

    feat: Adição de comentários e descrição README

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Mean-Variance-Standard Deviation Calculator
 
-This is the boilerplate for the Mean-Variance-Standard Deviation Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/data-analysis-with-python/data-analysis-with-python-projects/mean-variance-standard-deviation-calculator
+O seguinte repósitório tem a finalidade de apresentar o projeto número 1 do curso Data Analysis with Python.
+
+## Principais Funcionalidades
+
+- Cálculo de média
+- Cálculo de variância
+- Cálculo do desvio padrão
+- Identificação do máximo
+- Identificação do mínimo
+- Cálculo da soma das linhas, colunas e elementos em uma matriz 3 x 3.
--- mean_var_std.py
+++ mean_var_std.py
@@ -1,15 +1,19 @@
+## Importação de bibliotecas
+
 import numpy as np
 
-## gerar a média, a variância, o desvio padrão, o máximo, o mínimo e a soma das linhas, colunas e elementos em uma matriz 3 x 3.
+## Função para: gerar a média, a variância, o desvio padrão, o máximo, o mínimo e a soma das linhas, colunas e elementos em uma matriz 3 x 3.
 
 def calculate(list):
 
+    ### Validando se a lista contém 9 números
     if len(list) < 9:
         raise ValueError("List must contain nine numbers.")
     else:
         matriz = np.array(list).reshape(3,3)
         print(matriz)
         calculations = {
+            ### Manipulação de matrizes e cálculos estatísticos
             'mean': [np.mean(matriz, axis=0).tolist(), np.mean(matriz, axis=1).tolist(), np.mean(matriz).tolist()],
             'variance': [np.var(matriz, axis=0).tolist(), np.var(matriz, axis=1).tolist(), np.var(matriz).tolist()],
             'standard deviation': [np.std(matriz, axis=0).tolist(), np.std(matriz, axis=1).tolist(), np.std(matriz).tolist()],
